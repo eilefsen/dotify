61296cf76624b3256e17c684466c181ffadc6142
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-29 22:02:09 +0100

    add css class

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -17,7 +17,7 @@ const Slider = React.forwardRef<
         )}
         {...props}
     >
-        <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-slate-500">
+        <SliderPrimitive.Track className="slider-track relative h-2 w-full grow overflow-hidden rounded-full bg-slate-500">
             <SliderPrimitive.Range className="absolute h-full bg-slate-200" />
         </SliderPrimitive.Track>
         <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-slate-200 bg-slate-800 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
