9120e794cb2c1721d0c5a6b2e746300d2307438d
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-30 09:50:51 +0100

    fix cursor styling over slider thumb

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -6,22 +6,22 @@ import * as SliderPrimitive from "@radix-ui/react-slider"
 import { cn } from "@/lib/utils"
 
 const Slider = React.forwardRef<
-    React.ElementRef<typeof SliderPrimitive.Root>,
-    React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+	React.ElementRef<typeof SliderPrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, ...props }, ref) => (
-    <SliderPrimitive.Root
-        ref={ref}
-        className={cn(
-            "relative flex w-full touch-none select-none items-center",
-            className
-        )}
-        {...props}
-    >
-        <SliderPrimitive.Track className="slider-track relative h-2 w-full grow overflow-hidden rounded-full bg-slate-500">
-            <SliderPrimitive.Range className="absolute h-full bg-slate-200" />
-        </SliderPrimitive.Track>
-        <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-slate-200 bg-slate-800 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
-    </SliderPrimitive.Root>
+	<SliderPrimitive.Root
+		ref={ref}
+		className={cn(
+			"relative flex w-full touch-none select-none items-center",
+			className
+		)}
+		{...props}
+	>
+		<SliderPrimitive.Track className="slider-track relative h-2 w-full grow overflow-hidden rounded-full bg-slate-500">
+			<SliderPrimitive.Range className="absolute h-full bg-slate-200" />
+		</SliderPrimitive.Track>
+		<SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-slate-200 bg-slate-800 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-grab active:cursor-grabbing" />
+	</SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
 
