b38b6d3b17b74e0985d90100dc0ec626227c9152
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-02-08 18:58:20 +0100

    make main element overflow scroll

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
     return (
         <>
             <Footer />
-            <main className="content px-6 pt-2 fixed top-0 left-52 bottom-20 right-0">
+            <main className="content overflow-scroll px-6 pt-2 fixed top-0 left-52 bottom-20 right-0">
                 <Outlet />
             </main>
             <Sidebar />
