4c712b48dbe2aa7967cea4ec8e357e86c2b63d88
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-02-14 13:11:09 +0100

    move utils

---
+++ backend/utils/utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ParseUint32(s string) (uint32, error) {
+	n, err := strconv.ParseUint(s, 10, 32)
+	if err != nil {
+		return 0, fmt.Errorf("ParseUint32 %q: %v", n, err)
+	}
+	return uint32(n), nil
+}
