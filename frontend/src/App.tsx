30d41df3218e62f1a1a1d3cbb9565a62177f3d8c
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-02-15 11:09:30 +0100

    format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
 import './App.css';
 import {PlayerStore, iconsContext, playerStoreContext} from './components/player';
-import Layout from './components/layout';
 import {
-    createBrowserRouter,
     RouterProvider,
 } from 'react-router-dom';
 
 import {icons} from './icons';
-import Albums from './components/pages/Albums';
-import AlbumContent from './components/pages/Album';
-import Songs from './components/pages/Songs';
 import router from './router';
 
 
