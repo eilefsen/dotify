0fa60053c74ca72db78813848e472cfdd055b02e
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-02-16 11:06:22 +0100

    edit handlers, make api compliant to spec

--- backend/handlers.go
+++ backend/handlers.go
@@ -12,6 +12,11 @@ import (
 
 func FetchAllAlbums(w http.ResponseWriter, r *http.Request) {
 	albums, err := models.Albums{}.All()
+	if err == models.ErrResourceNotFound {
+		slog.Error("FetchAllAlbums: No albums found", "error", err)
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
 	if err != nil {
 		slog.Error(err.Error())
 	}
@@ -54,18 +59,25 @@ func FetchAlbumByID(w http.ResponseWriter, r *http.Request) {
 }
 
 func FetchAlbumsByArtist(w http.ResponseWriter, r *http.Request) {
-	name := chi.URLParam(r, "name")
+	id, err := utils.ParseUint32(chi.URLParam(r, "id"))
+	if err != nil {
+		slog.Error(err.Error())
+		// An error here means that the id argument is not parseable as uint32.
+		// Which is incorrect syntax, and therefore a Bad Request.
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-	artist, err := models.Artist{}.ByName(name)
+	artist, err := models.Artist{}.ByID(id)
 	if err == models.ErrResourceNotFound {
-		slog.Error("FetchAlbumsByArtist: No artist found", "artist", name, "error", err)
+		slog.Error("FetchAlbumsByArtist: No artist found", "artist", artist, "error", err)
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
 	albums, err := models.Albums{}.ByArtist(artist.ID)
 	if err == models.ErrResourceNotFound {
-		slog.Error("FetchAlbumsByArtist: No album found", "artist", name, "error", err)
+		slog.Error("FetchAlbumsByArtist: No album found", "artist", artist, "error", err)
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
@@ -74,7 +86,7 @@ func FetchAlbumsByArtist(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	slog.Debug("FetchAlbumByArtist:", "album", albums, "artist", name)
+	slog.Debug("FetchAlbumByArtist:", "album", albums, "artist", artist)
 
 	responseJSON, err := json.Marshal(albums)
 	if err != nil {
@@ -89,12 +101,38 @@ func FetchAlbumsByArtist(w http.ResponseWriter, r *http.Request) {
 
 func FetchAllSongs(w http.ResponseWriter, r *http.Request) {
 	songs, err := models.Songs{}.All()
+	if err == models.ErrResourceNotFound {
+		slog.Error("FetchAllSongs: No songs found", "error", err)
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
 	if err != nil {
 		slog.Error(err.Error())
 	}
 	slog.Debug("FetchAllSongs", "songs", songs)
 
 	responseJSON, err := json.Marshal(songs)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseJSON)
+}
+
+func FetchAllArtists(w http.ResponseWriter, r *http.Request) {
+	artists, err := models.Artists{}.All()
+	if err == models.ErrResourceNotFound {
+		slog.Error("FetchAllArtists: No artists found", "error", err)
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	slog.Debug("FetchAllArtists", "artists", artists)
+
+	responseJSON, err := json.Marshal(artists)
 	if err != nil {
 		slog.Error(err.Error())
 	}
