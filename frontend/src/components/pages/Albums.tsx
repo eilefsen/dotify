2bf67eaa0165e6804dd08471db71b9c2f1ff10ce
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-02-21 14:04:54 +0100

    mobile Album browsing page

--- frontend/src/components/pages/Albums.tsx
+++ frontend/src/components/pages/Albums.tsx
@@ -4,30 +4,53 @@ import {Link, useLoaderData} from "react-router-dom";
 import {Album} from "../player/types";
 
 export default function Albums() {
-    const albumCards: ReactNode[] = [];
+    const albumLines: ReactNode[] = [];
     const albums = useLoaderData() as Album[];
     console.debug(albums);
     albums.forEach((album) => {
-        albumCards.push(<AlbumCard key={album.id} title={album.title} artist={album.artist.name} imgSrc={album.imgSrc} to={`/album/${album.id}`} />);
+        albumLines.push(<AlbumLine key={album.id} title={album.title} artist={album.artist.name} imgSrc={album.imgSrc} to={`/album/${album.id}`} />);
     });
 
     return (
         <>
-            <div className="album-gallery flex flex-wrap gap-5">
-                {albumCards}
+            <div className="album-list">
+                {albumLines}
             </div>
         </>
     );
 }
 
-interface albumCardProps {
+interface AlbumLineProps {
     title: string,
     artist: string,
     imgSrc: string,
     to: string,
 }
 
-function AlbumCard({title, artist, imgSrc, to}: albumCardProps) {
+function AlbumLine({title, artist, imgSrc, to}: AlbumLineProps) {
+    return (
+        <Link to={to} className="album-line p-2 h-16 w-full bg-neutral-800 flex items-center">
+            <CoverImg src={imgSrc} alt={title} className="w-16 h-16" />
+            <div className="pl-2 font-bold">
+                <div className='text-neutral-300 font-bold text-base'>
+                    {title}
+                </div>
+                <div className='text-neutral-400 font-bold text-sm'>
+                    {artist}
+                </div>
+            </div>
+        </Link>
+    );
+}
+
+interface AlbumCardProps {
+    title: string,
+    artist: string,
+    imgSrc: string,
+    to: string,
+}
+
+function AlbumCard({title, artist, imgSrc, to}: AlbumCardProps) {
     return (
         <Link to={to} className="album-card w-48 rounded-2xl bg-white overflow-hidden">
             <CoverImg src={imgSrc} alt={title} />
