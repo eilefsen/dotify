85e6ada178b81108ad54fa330ce84225244919f1
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-02-21 19:35:54 +0100

    fix sticky touch hover

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
     content: [
         './pages/**/*.{ts,tsx}',
         './components/**/*.{ts,tsx}',
