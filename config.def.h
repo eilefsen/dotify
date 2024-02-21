5c19e23146b6163e6508765f0062cde9a1718be7
sewn <sewn@disroot.org>
2024-02-21 20:41:29 +0300

    switch to wmenu
    bemenu is very bloated, turning itself into a library, which makes it
    7489 SLOC.
    
    wmenu on the other hand, looks suckless by default, and is only 2000
    SLOC, which i also find alot nicer to use, since bemenu does nothing
    to replicate the original dmenu feel.

--- config.def.h
+++ config.def.h
@@ -115,7 +115,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "bemenu-run", NULL };
+static const char *menucmd[] = { "wmenu_run", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
