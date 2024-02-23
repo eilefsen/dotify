3168ce205f1ddb4469c3aa9426a3b87006b438cc
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-02-23 13:01:24 +0100

    adjust header styling

--- frontend/src/components/layout/mobile/LibraryHeader.tsx
+++ frontend/src/components/layout/mobile/LibraryHeader.tsx
@@ -6,7 +6,7 @@ import {NavLink} from "react-router-dom";
 export function LibraryHeader() {
     return (
         <>
-            <div className="library-header fixed top-0 left-0 right-0 h-14 z-20 bg-neutral-950 border-neutral-800 border-b flex gap-2 items-center justify-center">
+            <div className="library-header fixed top-0 left-0 right-0 h-14 z-20 bg-neutral-950 border-neutral-800 border-b flex gap-2 overflow-x-scroll px-2 items-center justify-center">
                 <LibraryBtn to="/albums" label="Albums" />
                 <LibraryBtn to="/artists" label="Artists" />
                 <LibraryBtn to="/songs" label="Songs" />
@@ -29,7 +29,7 @@ const LibraryBtn = observer(({to, label}: LibraryBtnProps) => {
         <NavLink to={to}
             onClick={onClick}
             className={({isActive}) => {
-                const base = "rounded-lg border-neutral-600 border px-4 py-1 h-10 text-xl flex items-center";
+                const base = "rounded-lg border-neutral-600 w-full justify-center border px-4 py-1 h-11 text-xl flex items-center";
                 if (isActive) {
                     return base + " bg-white text-neutral-800";
                 } else {
