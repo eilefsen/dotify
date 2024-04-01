2fb131bba6fe7d4cc75b1a1eeee01cc32023c853
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-01 15:57:05 +0200

    add login button

--- frontend/src/components/layout/mobile/LibraryHeader.tsx
+++ frontend/src/components/layout/mobile/LibraryHeader.tsx
@@ -1,41 +1,57 @@
-import {playerStoreContext} from "@/components/player";
-import {observer} from "mobx-react-lite";
-import {useContext} from "react";
-import {NavLink} from "react-router-dom";
+import { playerStoreContext } from "@/components/player";
+import { cn } from "@/lib/utils";
+import { observer } from "mobx-react-lite";
+import { PropsWithChildren, useContext } from "react";
+import { NavLink } from "react-router-dom";
 
 export function LibraryHeader() {
-    return (
-        <>
-            <div className="library-header fixed top-0 left-0 right-0 h-14 z-20 bg-neutral-950 border-neutral-800 border-b flex gap-2 overflow-x-scroll px-2 items-center justify-center">
-                <LibraryBtn to="/albums" label="Albums" />
-                <LibraryBtn to="/artists" label="Artists" />
-                <LibraryBtn to="/songs" label="Songs" />
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="library-header fixed left-0 right-0 top-0 z-20 flex h-14 items-center justify-center gap-2 overflow-x-scroll border-b border-neutral-800 bg-neutral-950 px-2">
+				<span className="flex w-full gap-2">
+					<LibraryBtn className="w-full" to="/albums">
+						Albums
+					</LibraryBtn>
+					<LibraryBtn className="w-full" to="/artists">
+						Artists
+					</LibraryBtn>
+					<LibraryBtn className="w-full" to="/songs">
+						Songs
+					</LibraryBtn>
+				</span>
+				<LibraryBtn to="/login">Login</LibraryBtn>
+			</div>
+		</>
+	);
 }
 
-interface LibraryBtnProps {
-    to: string;
-    label: string;
+interface LibraryBtnProps extends PropsWithChildren {
+	to: string;
+	className?: string;
 }
 
-const LibraryBtn = observer(({to, label}: LibraryBtnProps) => {
-    const player = useContext(playerStoreContext);
-    function onClick() {
-        player.isVisible = false;
-    }
-    return (
-        <NavLink to={to}
-            onClick={onClick}
-            className={({isActive}) => {
-                const base = "rounded-lg border-neutral-600 w-full justify-center border px-4 py-1 h-11 text-xl flex items-center";
-                if (isActive) {
-                    return base + " bg-white text-neutral-800";
-                } else {
-                    return base + " bg-neutral-800 text-white";
-                }
-            }}
-        >{label}</NavLink>
-    );
+const LibraryBtn = observer((props: LibraryBtnProps) => {
+	const player = useContext(playerStoreContext);
+	function onClick() {
+		player.isVisible = false;
+	}
+	return (
+		<NavLink
+			to={props.to}
+			onClick={onClick}
+			className={({ isActive }) => {
+				const base = cn(
+					"flex h-11 items-center justify-center rounded-lg border border-neutral-600 px-4 py-1 text-xl",
+					props.className,
+				);
+				if (isActive) {
+					return base + " bg-white text-neutral-800";
+				} else {
+					return base + " bg-neutral-800 text-white";
+				}
+			}}
+		>
+			{props.children}
+		</NavLink>
+	);
 });
