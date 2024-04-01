63826deb31a1ef6b6e61a94683a53553e6dd4355
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-01 16:31:05 +0200

    add login/logout icons

--- frontend/src/components/layout/mobile/LibraryHeader.tsx
+++ frontend/src/components/layout/mobile/LibraryHeader.tsx
@@ -1,4 +1,4 @@
-import { playerStoreContext } from "@/components/player";
+import { iconsContext, playerStoreContext } from "@/components/player";
 import { cn } from "@/lib/utils";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
@@ -7,19 +7,11 @@ import { PropsWithChildren, useContext } from "react";
 import { NavLink } from "react-router-dom";
 
 export function LibraryHeader() {
-	const queryClient = useQueryClient();
 	const isLoggedIn = useQuery({
 		queryKey: ["loginStatus"],
+		initialData: false,
 		enabled: false,
 	});
-	async function logout() {
-		const res = await axios.post("/api/auth/logout");
-		if (res.status == 200) {
-			console.log("Logged out!");
-			queryClient.setQueryData(["loginStatus"], false);
-		}
-		return res;
-	}
 	return (
 		<>
 			<div className="library-header fixed left-0 right-0 top-0 z-20 flex h-14 items-center justify-center gap-2 overflow-x-scroll border-b border-neutral-800 bg-neutral-950 px-2">
@@ -34,20 +26,42 @@ export function LibraryHeader() {
 						Songs
 					</LibraryBtn>
 				</span>
-				{
-					// inverted boolean because we want to show the login link when not logged in
-					!isLoggedIn.data ? (
-						<LibraryBtn to="/login">Login</LibraryBtn>
-					) : (
-						<button
-							className="flex h-11 items-center justify-center rounded-lg border border-neutral-600 px-4 py-1 text-xl"
-							onClick={logout}
-						>
-							Log out
-						</button>
-					)
-				}
+				<LoginButton isLoggedIn={isLoggedIn.data} />
 			</div>
+		</>
+	);
+}
+
+interface LoginButtonProps {
+	isLoggedIn: boolean;
+}
+function LoginButton(props: LoginButtonProps) {
+	const icons = useContext(iconsContext);
+	const queryClient = useQueryClient();
+	async function logout() {
+		const res = await axios.post("/api/auth/logout");
+		if (res.status == 200) {
+			console.log("Logged out!");
+			queryClient.setQueryData(["loginStatus"], false);
+		}
+		return res;
+	}
+	const classString =
+		"flex h-11 w-32 items-center justify-center rounded-lg border border-white bg-neutral-950 hover:bg-white hover:text-black px-4 py-1 text-xl transition-colors";
+	return (
+		<>
+			{
+				// inverted boolean because we want to show the login link when not logged in
+				!props.isLoggedIn ? (
+					<LibraryBtn className={classString} to="/login">
+						{icons.login}
+					</LibraryBtn>
+				) : (
+					<button className={classString} onClick={logout}>
+						{icons.logout}
+					</button>
+				)
+			}
 		</>
 	);
 }
--- frontend/src/components/player/context.tsx
+++ frontend/src/components/player/context.tsx
@@ -1,175 +1,199 @@
-import {computed, makeAutoObservable, runInAction} from "mobx";
-import {createContext} from "react";
+import { computed, makeAutoObservable, runInAction } from "mobx";
+import { createContext } from "react";
 
-import {PiGuitar, PiMusicNotesSimple, PiPauseCircleBold, PiPauseCircleFill, PiPlayCircleBold, PiPlayCircleFill, PiPlaylist, PiSkipBackFill, PiSkipForwardFill, PiSpeakerHigh, PiSpeakerLow, PiSpeakerNone, PiSpeakerSlash, PiTimer, PiVinylRecord} from "react-icons/pi";
-import {Song} from "./types";
+import {
+	PiGuitar,
+	PiMusicNotesSimple,
+	PiPauseCircleBold,
+	PiPauseCircleFill,
+	PiPlayCircleBold,
+	PiPlayCircleFill,
+	PiPlaylist,
+	PiSignIn,
+	PiSignOut,
+	PiSkipBackFill,
+	PiSkipForwardFill,
+	PiSpeakerHigh,
+	PiSpeakerLow,
+	PiSpeakerNone,
+	PiSpeakerSlash,
+	PiTimer,
+	PiVinylRecord,
+} from "react-icons/pi";
+import { Song } from "./types";
 
 const iconSize = 32;
 const playIconSize = 56;
 const skipIconSize = 24;
 const iconClassName = "hover:text-neutral-300 transition-colors duration-75";
 export const iconsContext = createContext({
-    iconSize: iconSize,
-    play: <PiPlayCircleFill size={playIconSize} className={iconClassName} />,
-    playOutline: <PiPlayCircleBold size={playIconSize} className={iconClassName} />,
-    playSmall: <PiPlayCircleFill size={iconSize} className={iconClassName} />,
-    pause: <PiPauseCircleFill size={playIconSize} className={iconClassName} />,
-    pauseOutline: <PiPauseCircleBold size={playIconSize} className={iconClassName} />,
-    pauseSmall: <PiPauseCircleFill size={iconSize} className={iconClassName} />,
-    prev: <PiSkipBackFill size={skipIconSize} className={iconClassName} />,
-    next: <PiSkipForwardFill size={skipIconSize} className={iconClassName} />,
-    volumeHigh: <PiSpeakerHigh size={iconSize} className={iconClassName} />,
-    volumeMedium: <PiSpeakerLow size={iconSize} className={iconClassName} />,
-    volumeLow: <PiSpeakerNone size={iconSize} className={iconClassName} />,
-    volumeMute: <PiSpeakerSlash size={iconSize} className={iconClassName} />,
-    album: <PiVinylRecord size={iconSize} className={iconClassName} />,
-    song: <PiMusicNotesSimple size={iconSize} className={iconClassName} />,
-    playlist: <PiPlaylist size={iconSize} className={iconClassName} />,
-    artist: <PiGuitar size={iconSize} className={iconClassName} />,
-    timer: <PiTimer size={iconSize} className={iconClassName} />,
+	iconSize: iconSize,
+	play: <PiPlayCircleFill size={playIconSize} className={iconClassName} />,
+	playOutline: (
+		<PiPlayCircleBold size={playIconSize} className={iconClassName} />
+	),
+	playSmall: <PiPlayCircleFill size={iconSize} className={iconClassName} />,
+	pause: <PiPauseCircleFill size={playIconSize} className={iconClassName} />,
+	pauseOutline: (
+		<PiPauseCircleBold size={playIconSize} className={iconClassName} />
+	),
+	pauseSmall: <PiPauseCircleFill size={iconSize} className={iconClassName} />,
+	prev: <PiSkipBackFill size={skipIconSize} className={iconClassName} />,
+	next: <PiSkipForwardFill size={skipIconSize} className={iconClassName} />,
+	volumeHigh: <PiSpeakerHigh size={iconSize} className={iconClassName} />,
+	volumeMedium: <PiSpeakerLow size={iconSize} className={iconClassName} />,
+	volumeLow: <PiSpeakerNone size={iconSize} className={iconClassName} />,
+	volumeMute: <PiSpeakerSlash size={iconSize} className={iconClassName} />,
+	album: <PiVinylRecord size={iconSize} className={iconClassName} />,
+	song: <PiMusicNotesSimple size={iconSize} className={iconClassName} />,
+	playlist: <PiPlaylist size={iconSize} className={iconClassName} />,
+	artist: <PiGuitar size={iconSize} className={iconClassName} />,
+	timer: <PiTimer size={iconSize} className={iconClassName} />,
+	login: <PiSignIn className={iconClassName} />,
+	logout: <PiSignOut className={iconClassName} />,
 });
 
 export class PlayerStore {
-    audio = new Audio();
-    isReady = false;
-    isPlaying = false;
-    isMuted = false;
-    volume = 0.7;
-    duration = 0;
-    seek = 0;
-    songIndex = -1;
-    songList: Song[] = [];
-    isVisible = false;
+	audio = new Audio();
+	isReady = false;
+	isPlaying = false;
+	isMuted = false;
+	volume = 0.7;
+	duration = 0;
+	seek = 0;
+	songIndex = -1;
+	songList: Song[] = [];
+	isVisible = false;
 
-    constructor(songs: Song[]) {
-        this.songList = songs;
-        this.audio.preload = "metadata";
-        if (this.songCount > 0) {
-            this.songIndex = 0;
-            this.audio.src = this.currentSong.src;
-        }
-        makeAutoObservable(this, {
-            audio: false,
-            songCount: computed,
-            currentSong: computed,
-        });
+	constructor(songs: Song[]) {
+		this.songList = songs;
+		this.audio.preload = "metadata";
+		if (this.songCount > 0) {
+			this.songIndex = 0;
+			this.audio.src = this.currentSong.src;
+		}
+		makeAutoObservable(this, {
+			audio: false,
+			songCount: computed,
+			currentSong: computed,
+		});
 
-        this.audio.addEventListener('ended', (ev) => {
-            runInAction(() => {
-                if (this.skip(1)) {
-                    this.play();
-                }
-            });
-        });
-        this.audio.addEventListener('durationchange', (ev) => {
-            const target = (ev.currentTarget as HTMLAudioElement);
-            runInAction(() => {
-                this.duration = target.duration;
-            });
-        });
-        this.audio.addEventListener('canplay', (ev) => {
-            const target = (ev.currentTarget as HTMLAudioElement);
-            runInAction(() => {
-                target.volume = this.volume;
-                this.isReady = true;
-            });
-        });
-        this.audio.addEventListener('timeupdate', (ev) => {
-            const target = (ev.currentTarget as HTMLAudioElement);
-            runInAction(() => {
-                this.seek = target.currentTime;
-            });
-        });
-    }
+		this.audio.addEventListener("ended", (ev) => {
+			runInAction(() => {
+				if (this.skip(1)) {
+					this.play();
+				}
+			});
+		});
+		this.audio.addEventListener("durationchange", (ev) => {
+			const target = ev.currentTarget as HTMLAudioElement;
+			runInAction(() => {
+				this.duration = target.duration;
+			});
+		});
+		this.audio.addEventListener("canplay", (ev) => {
+			const target = ev.currentTarget as HTMLAudioElement;
+			runInAction(() => {
+				target.volume = this.volume;
+				this.isReady = true;
+			});
+		});
+		this.audio.addEventListener("timeupdate", (ev) => {
+			const target = ev.currentTarget as HTMLAudioElement;
+			runInAction(() => {
+				this.seek = target.currentTime;
+			});
+		});
+	}
 
-    loadSong(song: Song) {
-        //TODO: optimize song list somehow
-        //TODO: also preload the next song in collection, so that the forward skip button works
-        if (this.songIndex < 0) {
-            this.songIndex = 0;
-        }
-        this.addSong(song);
-    }
+	loadSong(song: Song) {
+		//TODO: optimize song list somehow
+		//TODO: also preload the next song in collection, so that the forward skip button works
+		if (this.songIndex < 0) {
+			this.songIndex = 0;
+		}
+		this.addSong(song);
+	}
 
-    addSong(song: Song) {
-        this.addSongs([song]);
-    }
-    addSongs(songs: Song[]) {
-        this.songList.push(...songs);
-        console.debug(this.songList);
-    }
-    clearSongs() {
-        this.songList = [];
-        this.songIndex = -1;
-    }
+	addSong(song: Song) {
+		this.addSongs([song]);
+	}
+	addSongs(songs: Song[]) {
+		this.songList.push(...songs);
+		console.debug(this.songList);
+	}
+	clearSongs() {
+		this.songList = [];
+		this.songIndex = -1;
+	}
 
-    togglePlay() {
-        if (this.isPlaying) {
-            this.pause();
-        } else {
-            this.play();
-        }
-    }
-    play() {
-        this.audio.play();
-        this.isPlaying = true;
-    }
-    pause() {
-        this.audio.pause();
-        this.isPlaying = false;
-    }
+	togglePlay() {
+		if (this.isPlaying) {
+			this.pause();
+		} else {
+			this.play();
+		}
+	}
+	play() {
+		this.audio.play();
+		this.isPlaying = true;
+	}
+	pause() {
+		this.audio.pause();
+		this.isPlaying = false;
+	}
 
-    toggleMute() {
-        this.audio.muted = !this.audio.muted;
-        this.isMuted = !this.isMuted;
-    };
+	toggleMute() {
+		this.audio.muted = !this.audio.muted;
+		this.isMuted = !this.isMuted;
+	}
 
-    setVolume(vol: number) {
-        this.audio.volume = vol;
-        this.volume = vol;
-    }
-    setProgress(time: number) {
-        this.audio.currentTime = time;
-        this.seek = time;
-    }
+	setVolume(vol: number) {
+		this.audio.volume = vol;
+		this.volume = vol;
+	}
+	setProgress(time: number) {
+		this.audio.currentTime = time;
+		this.seek = time;
+	}
 
-    skip(increment: number): boolean {
-        return this.skipToIndex(this.songIndex + increment);
-    }
-    skipToIndex(index: number): boolean {
-        if (index > this.songCount - 1 || index < 0) {
-            return false;
-        }
-        this.songIndex = index;
-        this.audio.src = this.currentSong.src;
-        this.audio.load();
-        return true;
-    }
-    skipToID(id: string) {
-        for (let i = 0; i < this.songList.length; i++) {
-            const song = this.songList[i];
-            if (song.id == id) {
-                this.skipToIndex(i);
-            }
-        }
-    }
-    delayedPlay(ms: number = 500) {
-        setTimeout(() => {
-            this.play();
-        }, ms);
-    }
+	skip(increment: number): boolean {
+		return this.skipToIndex(this.songIndex + increment);
+	}
+	skipToIndex(index: number): boolean {
+		if (index > this.songCount - 1 || index < 0) {
+			return false;
+		}
+		this.songIndex = index;
+		this.audio.src = this.currentSong.src;
+		this.audio.load();
+		return true;
+	}
+	skipToID(id: string) {
+		for (let i = 0; i < this.songList.length; i++) {
+			const song = this.songList[i];
+			if (song.id == id) {
+				this.skipToIndex(i);
+			}
+		}
+	}
+	delayedPlay(ms: number = 500) {
+		setTimeout(() => {
+			this.play();
+		}, ms);
+	}
 
-    get songCount() {
-        return this.songList.length;
-    }
+	get songCount() {
+		return this.songList.length;
+	}
 
-    get currentSong() {
-        return this.songList[this.songIndex];
-    }
+	get currentSong() {
+		return this.songList[this.songIndex];
+	}
 
-    get isEmpty() {
-        return (this.songList.length == 0);
-    }
+	get isEmpty() {
+		return this.songList.length == 0;
+	}
 }
 
 export const playerStoreContext = createContext(new PlayerStore([]));
