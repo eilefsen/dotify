b3700d9ddfc29960cd21747ffcbd62814a909018
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-01 15:57:10 +0200

    add login route

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,50 +1,60 @@
-import {createBrowserRouter} from "react-router-dom";
-import {fetchAlbumByID, fetchAlbumsByArtist, fetchAllAlbums, fetchAllArtists, fetchAllSongs} from "./loaders";
-import Layout from "@/components/layout";
+import { createBrowserRouter } from "react-router-dom";
+import {
+	fetchAlbumByID,
+	fetchAlbumsByArtist,
+	fetchAllAlbums,
+	fetchAllArtists,
+	fetchAllSongs,
+} from "./loaders";
 import AlbumContent from "@/components/pages/Album";
 import Albums from "@/components/pages/Albums";
 import Songs from "@/components/pages/Songs";
 import Artists from "@/components/pages/Artists";
 import MobileLayout from "@/components/layout/mobile/MobileLayout";
+import Login from "@/components/pages/login/Login";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MobileLayout />,
-        children: [
-            {
-                path: "/albums/",
-                element: <Albums />,
-                loader: fetchAllAlbums,
-            },
-            {
-                path: "/album/:id",
-                element: <AlbumContent />,
-                errorElement: <></>,
-                loader: async ({params}) => {
-                    const id = parseInt(params.id!);
-                    return fetchAlbumByID(id);
-                }
-            },
-            {
-                path: "/artists",
-                element: <Artists />,
-                loader: fetchAllArtists,
-            },
-            {
-                path: "/albums/artist/:id",
-                element: <Albums />,
-                loader: async ({params}) => {
-                    const id = parseInt(params.id!);
-                    return fetchAlbumsByArtist(id);
-                }
-            },
-            {
-                path: "/songs/",
-                element: <Songs />,
-                loader: fetchAllSongs,
-            },
-        ],
-    },
+	{
+		path: "/",
+		element: <MobileLayout />,
+		children: [
+			{
+				path: "/albums/",
+				element: <Albums />,
+				loader: fetchAllAlbums,
+			},
+			{
+				path: "/album/:id",
+				element: <AlbumContent />,
+				errorElement: <></>,
+				loader: async ({ params }) => {
+					const id = parseInt(params.id!);
+					return fetchAlbumByID(id);
+				},
+			},
+			{
+				path: "/artists",
+				element: <Artists />,
+				loader: fetchAllArtists,
+			},
+			{
+				path: "/albums/artist/:id",
+				element: <Albums />,
+				loader: async ({ params }) => {
+					const id = parseInt(params.id!);
+					return fetchAlbumsByArtist(id);
+				},
+			},
+			{
+				path: "/songs/",
+				element: <Songs />,
+				loader: fetchAllSongs,
+			},
+			{
+				path: "/login/",
+				element: <Login />,
+			},
+		],
+	},
 ]);
 export default router;
