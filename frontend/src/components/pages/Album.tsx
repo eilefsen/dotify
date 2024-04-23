1e054ffc2f91b6a7cd71f42cfc0ca500ac9e115d
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-24 01:16:52 +0200

    format

--- frontend/src/components/pages/Album.tsx
+++ frontend/src/components/pages/Album.tsx
@@ -1,77 +1,79 @@
-import {useLoaderData} from "react-router-dom";
-import {SongTable} from "../songList";
-import {iconsContext, playerStoreContext} from "../player";
-import {useContext} from "react";
-import {AlbumWithSongs} from "../player/types";
-import {observer} from "mobx-react-lite";
-
+import { useLoaderData } from "react-router-dom";
+import { SongTable } from "../songList";
+import { iconsContext, playerStoreContext } from "../player";
+import { useContext } from "react";
+import { AlbumWithSongs } from "../player/types";
+import { observer } from "mobx-react-lite";
 
 export default function AlbumContent() {
-    const album = useLoaderData() as AlbumWithSongs;
-    console.debug("Album:", album);
+	const album = useLoaderData() as AlbumWithSongs;
+	console.debug("Album:", album);
 
-    return (
-        <div className="w-full overflow-hidden py-2">
-            <div className="px-4 flex items-center">
-                <AlbumPlayButton album={album} />
-                <div className="flex items-center">
-                    <div className="pl-4 h-full w-full">
-                        <div className="text-3xl font-bold">
-                            {album.title}
-                        </div>
-                        <div className="pl-1 text-neutral-400 text-2xl font-medium">
-                            {album.artist.name}
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <SongTable songs={album.songs} albumIndexing={true} />
-        </div >
-    );
+	return (
+		<div className="w-full overflow-hidden py-2">
+			<div className="flex items-center px-4">
+				<AlbumPlayButton album={album} />
+				<div className="flex items-center">
+					<div className="h-full w-full pl-4">
+						<div className="text-3xl font-bold">{album.title}</div>
+						<div className="pl-1 text-2xl font-medium text-neutral-400">
+							{album.artist.name}
+						</div>
+					</div>
+				</div>
+			</div>
+			<SongTable songs={album.songs} albumIndexing={true} />
+		</div>
+	);
 }
 
 interface albumPlayButtonProps {
-    album: AlbumWithSongs;
+	album: AlbumWithSongs;
 }
 
-const AlbumPlayButton = observer(({album}: albumPlayButtonProps) => {
-    const player = useContext(playerStoreContext);
-    const icons = useContext(iconsContext);
+const AlbumPlayButton = observer(({ album }: albumPlayButtonProps) => {
+	const player = useContext(playerStoreContext);
+	const icons = useContext(iconsContext);
 
-    function toggleIcon() {
-        if (player.currentSong == undefined) {
-            return icons.playOutline;
-        }
-        if (player.currentSong.album.id == album.id && player.isPlaying) {
-            return icons.pauseOutline;
-        } else {
-            return icons.playOutline;
-        }
-    }
-    const btnIcon = toggleIcon();
+	function toggleIcon() {
+		if (player.currentSong == undefined) {
+			return icons.playOutline;
+		}
+		if (player.currentSong.album.id == album.id && player.isPlaying) {
+			return icons.pauseOutline;
+		} else {
+			return icons.playOutline;
+		}
+	}
+	const btnIcon = toggleIcon();
 
-    return (
-        <div className="w-48 grid relative justify-items-center items-center rounded-lg overflow-hidden">
-            <img
-                className='aspect-square w-full h-full col-start-1 row-start-1 brightness-95'
-                src={album.imgSrc}
-                alt={album.title}
-            />
-            <button
-                className={"w-full h-full col-start-1 row-start-1 drop-shadow-lg flex justify-center items-center active:backdrop-brightness-95 hover:backdrop-brightness-95 [&>svg]:hover:text-current active:text-white hover:text-white text-neutral-100"}
-                onClick={() => {
-                    if (player.currentSong == undefined || (player.currentSong.album.id != album.id)) {
-                        player.clearSongs();
-                        player.addSongs(album.songs);
-                        player.skipToIndex(0);
-                        player.play();
-                    } else {
-                        player.togglePlay();
-                    }
-                }}
-            >
-                {btnIcon}
-            </button>
-        </div>
-    );
+	return (
+		<div className="relative grid w-48 items-center justify-items-center overflow-hidden rounded-lg">
+			<img
+				className="col-start-1 row-start-1 aspect-square h-full w-full brightness-95"
+				src={album.imgSrc}
+				alt={album.title}
+			/>
+			<button
+				className={
+					"col-start-1 row-start-1 flex h-full w-full items-center justify-center text-neutral-100 drop-shadow-lg hover:text-white hover:backdrop-brightness-95 active:text-white active:backdrop-brightness-95 [&>svg]:hover:text-current"
+				}
+				onClick={() => {
+					if (
+						player.currentSong == undefined ||
+						player.currentSong.album.id != album.id
+					) {
+						player.clearSongs();
+						player.addSongs(album.songs);
+						player.skipToIndex(0);
+						player.play();
+					} else {
+						player.togglePlay();
+					}
+				}}
+			>
+				{btnIcon}
+			</button>
+		</div>
+	);
 });
--- frontend/src/components/pages/Albums.tsx
+++ frontend/src/components/pages/Albums.tsx
@@ -1,72 +1,90 @@
-import {ReactNode, useContext} from "react";
-import {CoverImg, playerStoreContext} from "../player";
-import {Link, useLoaderData} from "react-router-dom";
-import {Album} from "../player/types";
-import {observer} from "mobx-react-lite";
+import { ReactNode, useContext } from "react";
+import { CoverImg, playerStoreContext } from "../player";
+import { Link, useLoaderData } from "react-router-dom";
+import { Album } from "../player/types";
+import { observer } from "mobx-react-lite";
 
-export default function Albums() {
-    const albumLines: ReactNode[] = [];
-    const albums = useLoaderData() as Album[];
-    console.debug(albums);
-    albums.forEach((album) => {
-        albumLines.push(<AlbumLine key={album.id} album={album} to={`/album/${album.id}`} />);
-    });
+interface AlbumsProps {
+	albums?: Album[];
+}
 
-    return (
-        <>
-            <div className="album-list">
-                {albumLines}
-            </div>
-        </>
-    );
+export default function Albums(props: AlbumsProps) {
+	const albumLines: ReactNode[] = [];
+	let albums;
+	if (props.albums) {
+		albums = props.albums;
+	} else {
+		albums = useLoaderData() as Album[];
+	}
+	console.debug(albums);
+	albums.forEach((album) => {
+		albumLines.push(
+			<AlbumLine key={album.id} album={album} to={`/album/${album.id}`} />,
+		);
+	});
+
+	return (
+		<>
+			<div className="album-list">{albumLines}</div>
+		</>
+	);
 }
 
 interface AlbumLineProps {
-    album: Album,
-    to: string,
+	album: Album;
+	to: string;
 }
 
-const AlbumLine = observer(({album, to}: AlbumLineProps) => {
-    const player = useContext(playerStoreContext);
-    var bgColor = "bg-neutral-950";
-    if (album.id == player.currentSong?.album.id) {
-        bgColor = "bg-neutral-900";
-    }
+const AlbumLine = observer(({ album, to }: AlbumLineProps) => {
+	const player = useContext(playerStoreContext);
+	var bgColor = "bg-neutral-950";
+	if (album.id == player.currentSong?.album.id) {
+		bgColor = "bg-neutral-900";
+	}
 
-    return (
-        <Link to={to} className={"album-line p-1 h-16 w-full border-b border-neutral-900 flex items-center active:bg-neutral-800" + " " + bgColor}>
-            <img
-                className='aspect-square h-full rounded border-neutral-300 border'
-                src={album.imgSrc}
-                alt={album.title}
-            />
-            <div className="pl-2">
-                <p className='text-neutral-50 font-bold text-base'>
-                    {album.title}
-                </p>
-                <p className='text-neutral-400 font-bold text-sm'>
-                    <span className="font-normal">Album · </span>{album.artist.name}
-                </p>
-            </div>
-        </Link >
-    );
+	return (
+		<Link
+			to={to}
+			className={
+				"album-line flex h-16 w-full items-center border-b border-neutral-900 p-1 active:bg-neutral-800" +
+				" " +
+				bgColor
+			}
+		>
+			<img
+				className="aspect-square h-full rounded border border-neutral-300"
+				src={album.imgSrc}
+				alt={album.title}
+			/>
+			<div className="pl-2">
+				<p className="text-base font-bold text-neutral-50">{album.title}</p>
+				<p className="text-sm font-bold text-neutral-400">
+					<span className="font-normal">Album · </span>
+					{album.artist.name}
+				</p>
+			</div>
+		</Link>
+	);
 });
 
 interface AlbumCardProps {
-    title: string,
-    artist: string,
-    imgSrc: string,
-    to: string,
+	title: string;
+	artist: string;
+	imgSrc: string;
+	to: string;
 }
 
-function AlbumCard({title, artist, imgSrc, to}: AlbumCardProps) {
-    return (
-        <Link to={to} className="album-card w-48 rounded-2xl bg-white overflow-hidden">
-            <CoverImg src={imgSrc} alt={title} />
-            <div className="px-6 py-1.5 font-bold">
-                <h4 className="text-black text-xl">{title}</h4>
-                <h5 className="text-neutral-800 ">{artist}</h5>
-            </div>
-        </Link>
-    );
+function AlbumCard({ title, artist, imgSrc, to }: AlbumCardProps) {
+	return (
+		<Link
+			to={to}
+			className="album-card w-48 overflow-hidden rounded-2xl bg-white"
+		>
+			<CoverImg src={imgSrc} alt={title} />
+			<div className="px-6 py-1.5 font-bold">
+				<h4 className="text-xl text-black">{title}</h4>
+				<h5 className="text-neutral-800 ">{artist}</h5>
+			</div>
+		</Link>
+	);
 }
