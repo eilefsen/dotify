873733eaa3525162c76418f6ee16590bdd1e57d2
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-23 23:35:14 +0200

    format

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,21 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
-    optimizeDeps: {
-        exclude: ['react-icons']
-    },
-    server: {
-        proxy: {
-            '/api': 'http://localhost:3000',
-        },
-    },
-})
-
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	optimizeDeps: {
+		exclude: ["react-icons"],
+	},
+	server: {
+		proxy: {
+			"/api": "http://localhost:3000",
+		},
+	},
+});
