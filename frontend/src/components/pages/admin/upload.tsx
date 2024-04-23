8035e9c136053da30729eed19a343a534c5afd19
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-24 00:01:32 +0200

    add progressbar to uploadform

--- frontend/src/components/pages/admin/upload.tsx
+++ frontend/src/components/pages/admin/upload.tsx
@@ -9,8 +9,10 @@ import {
 	Form,
 } from "@/components/ui/form";
 import { InputFile } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 
 export function Upload() {
 	return (
@@ -29,6 +31,7 @@ type AlbumUpload = {
 
 export function UploadForm() {
 	const form = useForm();
+	const [progress, setProgress] = useState(-1);
 
 	const mutation = useMutation({
 		mutationKey: ["audioFilesUpload"],
@@ -38,7 +41,14 @@ export function UploadForm() {
 				formData.append("audioFiles[]", audioFile);
 			}
 			formData.append("image", data.Image);
-			return axios.post("/api/admin/upload", formData);
+			return axios.post("/api/admin/upload", formData, {
+				onUploadProgress: (progressEvent) => {
+					let percentCompleted = Math.floor(
+						(progressEvent.loaded * 100) / progressEvent.total!,
+					);
+					setProgress(percentCompleted);
+				},
+			});
 		},
 		onSuccess: () => {
 			form.reset();
@@ -48,75 +58,90 @@ export function UploadForm() {
 	function onSubmit(values: any) {
 		mutation.mutate(values);
 	}
+	let progressBarClassName = "opacity-0";
+
+	if (progress > -1 && progress < 100) {
+		progressBarClassName = "";
+	}
 
 	let errorMsg;
+
+	if (mutation.isError) {
+		errorMsg = "Failed to upload";
+	}
 	return (
-		<Form {...form}>
-			<h2 className="-mb-0.5 text-2xl">Submit new Music</h2>
-			{errorMsg}
-			<form
-				onSubmit={form.handleSubmit(onSubmit)}
-				className="mx-auto w-full max-w-[30rem] space-y-4 text-left "
-			>
-				<FormField
-					control={form.control}
-					name="Audio"
-					render={({ field }) => {
-						return (
-							<FormItem>
-								<FormLabel hidden>Audio</FormLabel>
-								<FormControl>
-									<InputFile
-										name={field.name}
-										onBlur={field.onBlur}
-										disabled={field.disabled}
-										accept=".mp3"
-										id="audio"
-										value={field.value?.fileName}
-										onChange={(event) => {
-											field.onChange(event.target.files!);
-										}}
-										multiple
-									/>
-								</FormControl>
-								<FormDescription hidden>
-									This is the audio files of the album you are submitting
-								</FormDescription>
-								<FormMessage />
-							</FormItem>
-						);
-					}}
-				/>
-				<FormField
-					control={form.control}
-					name="Image"
-					render={({ field }) => {
-						return (
-							<FormItem>
-								<FormLabel hidden>Image</FormLabel>
-								<FormControl>
-									<InputFile
-										name={field.name}
-										onBlur={field.onBlur}
-										disabled={field.disabled}
-										accept=".jpg"
-										id="image"
-										value={field.value?.fileName}
-										onChange={(event) => {
-											field.onChange(event.target.files![0]);
-										}}
-									/>
-								</FormControl>
-								<FormDescription hidden>
-									This is the image for the album you are submitting
-								</FormDescription>
-								<FormMessage />
-							</FormItem>
-						);
-					}}
-				/>
-				<Button type="submit">Submit</Button>
-			</form>
-		</Form>
+		<div className="mx-auto w-fit">
+			<Form {...form}>
+				<h2 className="w-fit text-2xl">Submit new Music</h2>
+				<span className="text-red-500">{errorMsg}</span>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="mx-auto w-full max-w-[30rem] space-y-4 text-left "
+				>
+					<FormField
+						control={form.control}
+						name="Audio"
+						render={({ field }) => {
+							return (
+								<FormItem>
+									<FormLabel hidden>Audio</FormLabel>
+									<FormControl>
+										<InputFile
+											name={field.name}
+											onBlur={field.onBlur}
+											disabled={field.disabled}
+											accept=".mp3"
+											id="audio"
+											value={field.value?.fileName}
+											onChange={(event) => {
+												field.onChange(event.target.files!);
+											}}
+											multiple
+										/>
+									</FormControl>
+									<FormDescription hidden>
+										This is the audio files of the album you are submitting
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							);
+						}}
+					/>
+					<FormField
+						control={form.control}
+						name="Image"
+						render={({ field }) => {
+							return (
+								<FormItem>
+									<FormLabel hidden>Image</FormLabel>
+									<FormControl>
+										<InputFile
+											name={field.name}
+											onBlur={field.onBlur}
+											disabled={field.disabled}
+											accept=".jpg"
+											id="image"
+											value={field.value?.fileName}
+											onChange={(event) => {
+												field.onChange(event.target.files![0]);
+											}}
+										/>
+									</FormControl>
+									<FormDescription hidden>
+										This is the image for the album you are submitting
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							);
+						}}
+					/>
+					<Button type="submit">Submit</Button>
+				</form>
+			</Form>
+			<Progress
+				className={"transition-opacity " + progressBarClassName}
+				value={progress}
+			/>
+		</div>
 	);
 }
