e5fc181652619970a965768b3561e3f879f59cfd
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-24 00:02:06 +0200

    add volume slider to player

--- frontend/src/components/player/player.tsx
+++ frontend/src/components/player/player.tsx
@@ -1,51 +1,59 @@
-import {observer} from 'mobx-react-lite';
-import {useContext} from 'react';
+import { observer } from "mobx-react-lite";
+import { useContext } from "react";
 
-import {playerStoreContext} from './context';
+import { playerStoreContext } from "./context";
 import {
-    NextSongButton,
-    PlayButton,
-    PrevSongButton,
-    ProgressBar,
-} from './controls';
-import {SongTitle} from './status';
-import {Link} from 'react-router-dom';
-
+	MuteButton,
+	NextSongButton,
+	PlayButton,
+	PrevSongButton,
+	ProgressBar,
+	VolumeSlider,
+} from "./controls";
+import { SongTitle } from "./status";
+import { Link } from "react-router-dom";
 
 export const AudioPlayer = observer(() => {
-    const player = useContext(playerStoreContext);
+	const player = useContext(playerStoreContext);
 
-    function onClick() {
-        player.isVisible = !player.isVisible;
-    }
+	function onClick() {
+		player.isVisible = !player.isVisible;
+	}
 
-    return (
-        <div className="player px-12 w-full h-full text-xl text-center flex items-center">
-            <div className='flex flex-col gap-4 w-full self-center'>
-                <Link to={`/album/${player.currentSong.album.id}`}
-                    onClick={onClick}
-                    className="w-64 h-64 mx-auto"
-                >
-                    <img
-                        src={player.currentSong?.album.imgSrc}
-                        alt={player.currentSong?.album.title}
-                    />
-                </Link>
-                <SongTitle title={player.currentSong.title} artist={player.currentSong.artist.name} />
-                <ProgressBar />
-                <Transport />
-            </div>
-        </div>
-    );
+	return (
+		<div className="player flex h-full w-full items-center justify-center px-12 text-center text-xl">
+			<div className="flex w-full max-w-xl flex-col gap-4 self-center">
+				<Link
+					to={`/album/${player.currentSong.album.id}`}
+					onClick={onClick}
+					className="mx-auto h-64 w-64"
+				>
+					<img
+						src={player.currentSong?.album.imgSrc}
+						alt={player.currentSong?.album.title}
+					/>
+				</Link>
+				<SongTitle
+					title={player.currentSong.title}
+					artist={player.currentSong.artist.name}
+				/>
+				<ProgressBar />
+				<div className="flex gap-2">
+					<MuteButton />
+					<VolumeSlider />
+				</div>
+				<Transport />
+			</div>
+		</div>
+	);
 });
 
-
 function Transport() {
-    return (
-        <span className="transport-controls flex align-middle mx-auto justify-center gap-4 px-5 pb-2">
-            <PrevSongButton />
-            <PlayButton />
-            <NextSongButton />
-        </span>
-    );
+	return (
+		<span className="transport-controls mx-auto flex justify-center gap-4 px-5 pb-2 align-middle">
+			<PrevSongButton />
+			<PlayButton />
+			<NextSongButton />
+		</span>
+	);
 }
