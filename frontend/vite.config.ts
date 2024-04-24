c6aa9b91d5168e58a1c4a37355dd98f664835daf
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-24 20:10:32 +0200

    router plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), TanStackRouterVite()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
