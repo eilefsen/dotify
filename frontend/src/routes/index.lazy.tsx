cc6e0c64777b1328db233be6973c7c5c381b101a
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-25 19:38:43 +0200

    remove embarassing home page text

--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -5,9 +5,5 @@ export const Route = createLazyFileRoute("/")({
 });
 
 function Index() {
-	return (
-		<div className="p-2">
-			<h3>Welcome Home!</h3>
-		</div>
-	);
+	return <div className="p-2"></div>;
 }
