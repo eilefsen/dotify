23f859777e5064459bc48a6d695fbd078533706c
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-25 15:45:00 +0200

    add handler

--- backend/handlers.go
+++ backend/handlers.go
@@ -186,6 +186,51 @@ func FetchArtist(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(responseJSON)
 }
+
+func UpdateArtist(w http.ResponseWriter, r *http.Request) {
+	id, err := ParseUint32(chi.URLParam(r, "id"))
+	if err != nil {
+		slog.Error(err.Error())
+		// An error here means that the id argument is not parseable as uint32.
+		// Which is incorrect syntax, and therefore a Bad Request.
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var artist models.Artist
+	artist.ID = id
+
+	slog.Debug("UpdateArtist:", "artist", artist)
+
+	err = json.NewDecoder(r.Body).Decode(&artist)
+	if err != nil {
+		// If the structure of the body is wrong, return an HTTP error
+		slog.Error(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	slog.Debug("UpdateArtist:", "artist", artist)
+
+	err = artist.Update()
+	if err != nil {
+		slog.Error("UpdateArtist: ", "artist", artist, "error", err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	slog.Debug("FetchArtist:", "artist", artist)
+
+	responseJSON, err := json.Marshal(artist)
+	if err != nil {
+		slog.Error(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseJSON)
+}
+
 func FetchAllSongs(w http.ResponseWriter, r *http.Request) {
 	songs, err := models.Songs{}.All()
 	if err == models.ErrResourceNotFound {
