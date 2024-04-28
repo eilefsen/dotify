3ca52cd5fdb789c32971a2e269e99166ae9d3d99
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-28 17:36:02 +0200

    add delete capability to frontend

--- frontend/src/routes/admin/albums/index.tsx
+++ frontend/src/routes/admin/albums/index.tsx
@@ -1,11 +1,15 @@
-import { Album } from "@/components/player/types";
+import { Album, ArtistNoID } from "@/components/player/types";
 import {
 	ReactNode,
 	createFileRoute,
 	useLoaderData,
+	useRouter,
 } from "@tanstack/react-router";
 import axios from "axios";
 import { AlbumLine } from "@/routes/albums";
+import { PiTrashFill } from "react-icons/pi";
+import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
 
 export const Route = createFileRoute("/admin/albums/")({
 	component: AlbumAdmin,
@@ -30,13 +34,31 @@ interface AlbumsListProps {
 
 function AlbumsList(props: AlbumsListProps) {
 	const albumLines: ReactNode[] = [];
+
+	const router = useRouter();
+
+	const mutation = useMutation({
+		mutationKey: ["deleteAlbum"],
+		mutationFn: async (id: string) => {
+			const res = await axios.delete(`/api/admin/albums/${id}`);
+			return res;
+		},
+		onSuccess: () => {
+			router.invalidate();
+		},
+	});
+
 	props.albums.forEach((album) => {
 		albumLines.push(
-			<AlbumLine
-				key={album.id}
-				album={album}
-				to={`/admin/albums/${album.id}`}
-			/>,
+			<div className="flex items-center gap-2" key={album.id}>
+				<AlbumLine album={album} to={`/admin/albums/${album.id}`} />
+				<Button
+					className="border-none bg-white p-1 text-red-600 hover:bg-neutral-100 active:bg-neutral-200 active:text-red-500"
+					onClick={() => mutation.mutate(album.id)}
+				>
+					<PiTrashFill size={28} />
+				</Button>
+			</div>,
 		);
 	});
 
