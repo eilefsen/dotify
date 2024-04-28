8df471f4b82f1683c22bcd2846c4c9c3f8d59cde
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-28 20:11:29 +0200

    make pwa compatible

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
+	plugins: [
+		react(),
+		TanStackRouterVite(),
+		VitePWA({
+			manifest: {
+				name: "Music Player for the web",
+				short_name: "Dotify",
+				start_url: "/",
+				background_color: "#000000",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "/images/apple-touch-icon-120x120.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/images/icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
