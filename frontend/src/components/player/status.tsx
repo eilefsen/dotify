ca01d2eeed4b768d6ab49acc85cbde8e8e2e3b71
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-30 00:23:31 +0200

    full player colors

--- frontend/src/components/player/status.tsx
+++ frontend/src/components/player/status.tsx
@@ -1,22 +1,22 @@
-import {observer} from 'mobx-react-lite';
+import { observer } from "mobx-react-lite";
 
 interface SongTitleProps {
-    title?: string;
-    artist?: string;
+	title?: string;
+	artist?: string;
 }
 
-export const SongTitle = observer(function ({title, artist}: SongTitleProps) {
-    return (
-        <div className='song-title'>
-            <p className='text-neutral-200 font-bold'>
-                {title || "No Song playing"}
-                {artist && (
-                    <span className='text-neutral-400 font-normal'>
-                        <br />
-                        {artist}
-                    </span>
-                )}
-            </p>
-        </div >
-    );
+export const SongTitle = observer(function ({ title, artist }: SongTitleProps) {
+	return (
+		<div className="song-title">
+			<p className="font-bold text-foreground">
+				{title || "No Song playing"}
+				{artist && (
+					<span className="font-normal text-muted-foreground">
+						<br />
+						{artist}
+					</span>
+				)}
+			</p>
+		</div>
+	);
 });
