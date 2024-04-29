68504993362a7a380f13d98c8e72610cbe2f4373
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-29 15:09:44 +0200

    fix pwa updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 const queryClient = new QueryClient();
 
