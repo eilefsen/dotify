aee6c5a391b0b3b9eb33f735d358fea856ca9e73
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-30 01:08:17 +0200

    fix tailwind cfg

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,20 +17,6 @@ module.exports = {
 			},
 		},
 		extend: {
-			animation: {
-				marquee: "marquee 15s linear infinite",
-				marquee2: "marquee2 15s linear infinite",
-			},
-			keyframes: {
-				marquee: {
-					"0%": { transform: "translateX(0%)" },
-					"100%": { transform: "translateX(-100%)" },
-				},
-				marquee2: {
-					"0%": { transform: "translateX(100%)" },
-					"100%": { transform: "translateX(0%)" },
-				},
-			},
 			colors: {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
@@ -80,13 +66,22 @@ module.exports = {
 					from: { height: "var(--radix-accordion-content-height)" },
 					to: { height: "0" },
 				},
+				marquee: {
+					"0%": { transform: "translateX(0%)" },
+					"100%": { transform: "translateX(-100%)" },
+				},
+				marquee2: {
+					"0%": { transform: "translateX(100%)" },
+					"100%": { transform: "translateX(0%)" },
+				},
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
+				marquee: "marquee 15s linear infinite",
+				marquee2: "marquee2 15s linear infinite",
 			},
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
 };
-
