bd1e46d4e0978c974556f47ddf6698b0f0f2cc02
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-02 21:42:09 +0200

    add login queries to frontend

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,9 @@
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
 import { playerStoreContext, AudioPlayer } from "@/components/player";
+import { useQueryClient, useQuery } from "@tanstack/react-query";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import axios from "axios";
 import { observer } from "mobx-react-lite";
 import { useContext } from "react";
 
@@ -17,6 +19,47 @@ export const Route = createRootRoute({
 
 const LibraryOutlet = observer(() => {
 	const player = useContext(playerStoreContext);
+	const queryClient = useQueryClient();
+	const loginResult = useQuery({
+		queryKey: ["loginStatus"],
+		queryFn: async () => {
+			const res = await axios.post(`/api/auth/status`, {
+				validateStatus: () => true,
+			});
+			const ok = res.status == 200;
+			if (ok) {
+				console.info("Your access token is valid!");
+			}
+			return ok;
+		},
+		initialData: false,
+		retry: false,
+		enabled: true,
+	});
+	useQuery({
+		queryKey: ["refresh"],
+		queryFn: async () => {
+			const res = await axios.post(`/api/auth/refresh`, {
+				withCredentials: true,
+			});
+			const ok = res.status == 200;
+			if (ok) {
+				queryClient.setQueryData(["loginStatus"], true);
+				console.info("Access token refreshed!");
+			}
+			return ok;
+		},
+		refetchInterval: (query) => {
+			const status = query.state.status;
+			if (status == "error") {
+				return false;
+			}
+			return 240000;
+		},
+		retry: false,
+		enabled: true,
+	});
+
 	return (
 		<main
 			className={
