177b07e1b57928cb9c1bbc9b5529c66e8fdbe088
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-02 15:42:03 +0200

    add playlist table

---
+++ backend/models/sql/migrations/003_playlists.sql
@@ -0,0 +1,17 @@
+CREATE TABLE
+  `playlist` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `user_id` BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  );
+
+CREATE TABLE
+  `playlist_song` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `song_id` INT UNSIGNED NOT NULL,
+    `playlist_id` INT UNSIGNED NOT NULL,
+    FOREIGN KEY (`song_id`) REFERENCES `song` (`id`),
+    FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`)
+  );
+
