427c4671cf2b43c7830e65b7de9e65e67ff1c78e
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-04 14:22:59 +0200

    conditionally render a drawer instead of dialog on mobile

--- frontend/src/components/deleteDialog.tsx
+++ frontend/src/components/deleteDialog.tsx
@@ -10,6 +10,16 @@ import {
 } from "@/components/ui/dialog";
 import { MouseEventHandler } from "react";
 import { Trash2Icon } from "lucide-react";
+import { isDesktop } from "react-device-detect";
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer";
 
 interface DeleteDialogProps {
 	kind: string;
@@ -18,36 +28,71 @@ interface DeleteDialogProps {
 }
 
 export function DeleteDialog(props: DeleteDialogProps) {
+	if (isDesktop) {
+		return (
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button variant="destructiveHover" type="submit" className="flex">
+						<Trash2Icon />
+					</Button>
+				</DialogTrigger>
+				<DialogContent className="bg-background sm:max-w-md">
+					<DialogHeader>
+						<DialogTitle>Delete {props.kind}</DialogTitle>
+						<DialogDescription>
+							Are you sure you want to delete <em>"{props.name}"</em>
+						</DialogDescription>
+					</DialogHeader>
+					<div className="flex items-center space-x-2">
+						<Button
+							type="submit"
+							onClick={props.onClick}
+							className="flex"
+							variant="destructiveHover"
+						>
+							<Trash2Icon />
+						</Button>
+						<DialogClose asChild>
+							<Button type="submit" className="flex" variant="outline">
+								No
+							</Button>
+						</DialogClose>
+					</div>
+				</DialogContent>
+			</Dialog>
+		);
+	}
 	return (
-		<Dialog>
-			<DialogTrigger asChild>
+		<Drawer>
+			<DrawerTrigger asChild>
 				<Button variant="destructiveHover" type="submit" className="flex">
 					<Trash2Icon />
 				</Button>
-			</DialogTrigger>
-			<DialogContent className="bg-background sm:max-w-md">
-				<DialogHeader>
-					<DialogTitle>Delete {props.kind}</DialogTitle>
-					<DialogDescription>
+			</DrawerTrigger>
+			<DrawerContent className="bg-background px-4 pb-2">
+				<DrawerHeader>
+					<DrawerTitle>Delete {props.kind}</DrawerTitle>
+					<DrawerDescription>
 						Are you sure you want to delete <em>"{props.name}"</em>
-					</DialogDescription>
-				</DialogHeader>
-				<div className="flex items-center space-x-2">
+					</DrawerDescription>
+				</DrawerHeader>
+				<div className="flex flex-col gap-2">
 					<Button
 						type="submit"
 						onClick={props.onClick}
-						className="flex"
+						className="flex w-full gap-2"
 						variant="destructiveHover"
 					>
 						<Trash2Icon />
+						Delete
 					</Button>
-					<DialogClose asChild>
-						<Button type="submit" className="flex" variant="outline">
-							No
+					<DrawerClose asChild>
+						<Button type="submit" className="flex w-full" variant="outline">
+							Cancel
 						</Button>
-					</DialogClose>
+					</DrawerClose>
 				</div>
-			</DialogContent>
-		</Dialog>
+			</DrawerContent>
+		</Drawer>
 	);
 }
