611b04ce042e80c88abbbbfb6d528be8758b51dc
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-04 21:59:35 +0200

    format

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,5 +1,3 @@
-import { NavBar } from "../navbar";
-
 export function Header() {
 	return (
 		<div className="fixed left-0 right-0 top-0 z-20 flex h-14 items-center justify-center gap-2 overflow-hidden border-b border-primary bg-hover px-2 pt-1"></div>
