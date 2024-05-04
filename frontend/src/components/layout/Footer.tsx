82e5296e63307636b0efff1c59ec2a0d02865e06
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-04 21:52:39 +0200

    add shadow to footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -42,7 +42,7 @@ export const MiniPlayer = observer(() => {
 
 export function Footer() {
 	return (
-		<div className="fixed bottom-0 left-0 right-0 bg-secondary pb-4">
+		<div className="fixed bottom-0 left-0 right-0 bg-secondary pb-4 shadow-sm shadow-black -shadow-y-0.5 shadow-blur-3">
 			<MiniPlayer />
 			<NavBar />
 		</div>
