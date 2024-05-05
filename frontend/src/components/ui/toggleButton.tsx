b10bf84f03aa78453eba6031a5113149cc45f96d
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-05-05 13:37:36 +0200

    format

--- frontend/src/components/ui/toggleButton.tsx
+++ frontend/src/components/ui/toggleButton.tsx
@@ -1,23 +1,25 @@
+import { cn } from "@/lib/utils";
+
 interface ToggleButtonProps {
-    onClick: React.MouseEventHandler;
-    toggleState: boolean;
-    isDisabled?: boolean;
-    iconOn: React.ReactNode;
-    iconOff: React.ReactNode;
-    className?: string;
+	onClick: React.MouseEventHandler;
+	toggleState: boolean;
+	isDisabled?: boolean;
+	iconOn: React.ReactNode;
+	iconOff: React.ReactNode;
+	className?: string;
 }
 
 export default function ToggleButton(props: ToggleButtonProps) {
-    const {onClick, toggleState, isDisabled, iconOn, iconOff, className} = props;
-    return (
-        <button
-            className={"align-middle " + className}
-            disabled={isDisabled}
-            onClick={onClick}
-            aria-label={toggleState ? "on" : "off"}
-        >
-            {toggleState ? iconOn : iconOff}
-        </button>
-    );
-
+	const { onClick, toggleState, isDisabled, iconOn, iconOff, className } =
+		props;
+	return (
+		<button
+			className={cn("cursor-pointer align-middle", className)}
+			disabled={isDisabled}
+			onClick={onClick}
+			aria-label={toggleState ? "on" : "off"}
+		>
+			{toggleState ? iconOn : iconOff}
+		</button>
+	);
 }
