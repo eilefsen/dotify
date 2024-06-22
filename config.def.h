4a7d1bebf5c706109c92bd0415ab62825a7556ee
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-06-22 00:21:17 -0600

    add bugref for negative x,y monitor position and xwayland

--- config.def.h
+++ config.def.h
@@ -36,8 +36,10 @@ static const Layout layouts[] = {
 };
 
 /* monitors */
-/* (x=-1, y=-1) is reserved as an "autoconfigure" monitor position indicator */
-/* WARNING: negative values other than (-1, -1) cause problems with xwayland clients' menus */
+/* (x=-1, y=-1) is reserved as an "autoconfigure" monitor position indicator
+ * WARNING: negative values other than (-1, -1) cause problems with Xwayland clients
+ * https://gitlab.freedesktop.org/xorg/xserver/-/issues/899
+*/
 /* NOTE: ALWAYS add a fallback rule, even if you are completely sure it won't be used */
 static const MonitorRule monrules[] = {
 	/* name       mfact  nmaster scale layout       rotate/reflect                x    y */
